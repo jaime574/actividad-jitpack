3a2783cf68f6189a1fd2881e59523bed42d2ca77
Jaime Díaz Gómez <jaimezh2@gmail.com>
2020-04-02 19:58:20 +0200

    primer push

---
+++ jitpack/src/main/java/Copyright.java
@@ -0,0 +1,12 @@
+public class Copyright {
+    private String text;
+
+    public Copyright(String text){
+        this.text = text;
+    }
+
+    public String getText(){
+        return text;
+    }
+
+}
